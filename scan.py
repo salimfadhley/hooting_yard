49354cb5a1141effb0af6a861b8f1bae192a6442
Salim Fadhley <salimfadhley@gmail.com>
2020-01-12 22:31:12 +0000

    first commit

---
+++ scan.py
@@ -0,0 +1,23 @@
+import os
+import subprocess
+from git import Repo
+
+def main():
+    repo = Repo(".")
+
+    master = repo.heads.master
+    origin = repo.remotes.origin
+
+    for f in repo.untracked_files:
+        if f.endswith(".mp3"):
+            print(f)
+
+            repo.index.add([f])
+
+            repo.index.commit(f"Automatically added {f}.")
+
+
+            break
+
+if __name__ == "__main__":
+    main()
